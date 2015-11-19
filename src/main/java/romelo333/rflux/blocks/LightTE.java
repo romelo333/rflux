82c4c644ccf7f10a62bc172c34685b6bfc0769f8
McJty <mcjty1@gmail.com>
2015-11-19 21:19:52 +0100

    Packet stuff

--- src/main/java/romelo333/rflux/blocks/LightTE.java
+++ src/main/java/romelo333/rflux/blocks/LightTE.java
@@ -1,5 +1,9 @@
 package romelo333.rflux.blocks;
 
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 public class LightTE extends TileEntity {
@@ -12,4 +16,17 @@ public class LightTE extends TileEntity {
     public boolean canUpdate() {
         return false;
     }
+
+    @Override
+    public Packet getDescriptionPacket() {
+        NBTTagCompound nbtTag = new NBTTagCompound();
+        this.writeToNBT(nbtTag);
+        return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, nbtTag);
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity packet) {
+        readFromNBT(packet.func_148857_g());
+    }
+
 }
