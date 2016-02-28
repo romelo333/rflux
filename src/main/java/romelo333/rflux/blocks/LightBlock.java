cfd84520f4e7b1446df2fa755f22ae4ea06d1696
McJty <mcjty1@gmail.com>
2016-02-28 13:00:12 +0100

    Made more general

--- src/main/java/romelo333/rflux/blocks/LightBlock.java
+++ src/main/java/romelo333/rflux/blocks/LightBlock.java
@@ -1,56 +1,25 @@
 package romelo333.rflux.blocks;
 
 
-import net.minecraft.block.Block;
-import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.particle.EffectRenderer;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import romelo333.rflux.RFLux;
 
-import java.util.List;
 import java.util.Random;
 
-public class LightBlock extends Block implements ITileEntityProvider {
+public class LightBlock extends GenericLight {
 
-    public static PropertyBool LIT = PropertyBool.create("lit");
     public static PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 
     public LightBlock() {
-        super(Material.portal);
-        setHardness(0.0f);
-        setUnlocalizedName("flatlight");
-        setRegistryName("flatlight");
-        setCreativeTab(RFLux.tabRFLux);
-        setStepSound(Block.soundTypeCloth);
-        GameRegistry.registerBlock(this);
-        GameRegistry.registerTileEntity(LightTE.class, RFLux.MODID + ":flatlight");
+        super("lightblock", LightTE.class);
     }
-
-    @SideOnly(Side.CLIENT)
-    public void initModel() {
-        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
-    }
-
 
     @Override
     public int quantityDropped(Random rnd) {
@@ -65,53 +34,6 @@ public class LightBlock extends Block implements ITileEntityProvider {
     @Override
     public TileEntity createTileEntity(World world, IBlockState state) {
         return new LightTE();
-    }
-
-    @Override
-    public int getLightValue() {
-        return 15;
-    }
-
-    @Override
-    public int getLightValue(IBlockAccess world, BlockPos pos) {
-        IBlockState blockState = world.getBlockState(pos);
-        if (blockState.getValue(LIT)) {
-            return getLightValue();
-        } else {
-            return 0;
-        }
-    }
-
-    @Override
-    public void addCollisionBoxesToList(World worldIn, BlockPos pos, IBlockState state, AxisAlignedBB mask, List<AxisAlignedBB> list, Entity collidingEntity) {
-    }
-
-
-    @Override
-    public boolean shouldSideBeRendered(IBlockAccess worldIn, BlockPos pos, EnumFacing side) {
-        return false;
-    }
-
-    @Override
-    public boolean isBlockNormalCube() {
-        return false;
-    }
-
-    @Override
-    public boolean isOpaqueCube() {
-        return false;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean addHitEffects(World worldObj, MovingObjectPosition target, EffectRenderer effectRenderer) {
-        return true;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean addDestroyEffects(World world, BlockPos pos, EffectRenderer effectRenderer) {
-        return true;
     }
 
     @Override
