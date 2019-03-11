c48edb4cdef2ddbbbdaea806e8bb431986dc92f8
McJty <mcjty1@gmail.com>
2019-03-11 11:27:10 +0100

    New ClientRegistration class

--- src/main/java/romelo333/rflux/setup/ClientProxy.java
+++ src/main/java/romelo333/rflux/setup/ClientProxy.java
@@ -1,13 +1,9 @@
 package romelo333.rflux.setup;
 
 import mcjty.lib.setup.DefaultClientProxy;
-import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.obj.OBJLoader;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import romelo333.rflux.ClientEventHandler;
 import romelo333.rflux.ModBlocks;
 import romelo333.rflux.RFLux;
@@ -16,36 +12,10 @@ public class ClientProxy extends DefaultClientProxy {
 
     @Override
     public void preInit(FMLPreInitializationEvent e) {
-        MinecraftForge.EVENT_BUS.register(this);
         super.preInit(e);
         OBJLoader.INSTANCE.addDomain(RFLux.MODID);
         ClientEventHandler.subscribeMe();
     }
-
-    @Override
-    public void init(FMLInitializationEvent e) {
-        super.init(e);
-    }
-
-    @SubscribeEvent
-    public void registerModels(ModelRegistryEvent event) {
-        ModBlocks.initModels();
-    }
-
-
-//    @SubscribeEvent
-//    public void renderWorldLastEvent(RenderWorldLastEvent evt) {
-//        Minecraft mc = Minecraft.getMinecraft();
-//        EntityClientPlayerMP p = mc.thePlayer;
-//        ItemStack heldItem = p.getHeldItem();
-//        if (heldItem == null) {
-//            return;
-//        }
-//        if (heldItem.getItem() instanceof GenericWand) {
-//            GenericWand genericWand = (GenericWand) heldItem.getItem();
-//            genericWand.renderOverlay(evt, p, heldItem);
-//        }
-//    }
 
     @Override
     public void postInit(FMLPostInitializationEvent e) {
